c1182d5c0f4c30e4aef6ee84096b8d0383a8b49a
Cristopher Estrada <crissin21_01@hotmail.com>
2019-07-12 14:54:21 -0500

    search creado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,6 +9,7 @@ export class ProductosService {
   
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = []
 
 
   constructor(private http: HttpClient) { 
@@ -17,14 +18,53 @@ export class ProductosService {
     this.cargarProductos();
   }
 
-  private cargarProductos(){
-    this.http.get('https://angular-udemy-93d4b.firebaseio.com/productos_idx.json')
-         .subscribe((resp: Producto[]) => {
-                console.log(resp);
-                this.productos = resp;
-                this.cargando = false;
-              });
+  private cargarProductos() {
+
+    return new Promise( (resolve, reject) => {
+      this.http.get('https://angular-udemy-93d4b.firebaseio.com/productos_idx.json')
+           .subscribe((resp: Producto[]) => {
+                  this.productos = resp;
+                  this.cargando = false;
+                  resolve();
+                });
+
+    });
+
   }
 
+  getProducto( id: string){
+    return this.http.get(`https://angular-udemy-93d4b.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string){
+
+    if(this.productos.length === 0){
+      this.cargarProductos().then(()=>{
+
+        this.filtrarProductos(termino);
+      });
+    }else{
+      this.filtrarProductos(termino);
+    }
+
+    this.productosFiltrado = this.productos.filter( producto => {
+      return true;
+    });
+    // console.log(this.productosFiltrado);
+  }
+  private filtrarProductos(termino: string){
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0) {
+        this.productosFiltrado.push(prod);
+      }
+    });
+    
+  }
 
 }
